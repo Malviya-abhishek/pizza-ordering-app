cd76bfca952252afd9cfb721d17f3d30e22dd9e0
abhishek <abhismalviya@outlook.com>
2021-05-30 22:29:11 +0530

    login and register page

--- server.js
+++ server.js
@@ -11,12 +11,27 @@ const PORT = process.env.PORT || 3000;
 //Assets
 app.use(express.static('public'));
 
-// app.use(expressLayout);
+
+//Set template engine
+app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
 
+//routes
 app.get('', (req, res) =>{
   res.render('home')
+});
+
+app.get('/cart', (req, res) =>{
+  res.render('customers/cart')
+});
+
+app.get('/login', (req, res) =>{
+  res.render('auth/login')
+});
+
+app.get('/register', (req, res) =>{
+  res.render('auth/register')
 });
 
 app.listen(PORT, () => {
